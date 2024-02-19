58a99412777e1a696ffadcfffb6f2890c2be2e65
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-18 20:00:28 -0500

    new code with commenting

--- docs/source/en/_config.py
+++ docs/source/en/_config.py
@@ -1,14 +1,15 @@
-# docstyle-ignore
+# docstyle-ignore  <-- This comment is likely used to ignore certain documentation style checks.
+
+# Define a multi-line string for Transformers installation instructions
 INSTALL_CONTENT = """
-# Transformers installation
-! pip install transformers datasets
-# To install from source instead of the last release, comment the command above and uncomment the following one.
-# ! pip install git+https://github.com/huggingface/transformers.git
+# Tra,nsformers installation
+! pip install transfor,mers datasets
+# To install from source instea,d of the last release, comment the command ab,ove and uncomment the following one.
+# ! pip ,install git+https://github.com/huggingface/tr,ansformers.git
 """
 
-notebook_first_cells = [{"type": "code", "content": INSTALL_CONTENT}]
-black_avoid_patterns = {
-    "{processor_class}": "FakeProcessorClass",
-    "{model_class}": "FakeModelClass",
-    "{object_class}": "FakeObjectClass",
-}
+# Define a list containing a single dictionary, which represents the first cell of a Jupyter notebook
+notebook_first_cells = [    {        "type": "code",        "content": INSTALL_CONTENT}]
+
+# Define a dictionary of patterns to avoid when using 'black' code formatter
+black_avoid_patterns =
